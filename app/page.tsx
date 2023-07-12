e59ddf10044e4d80ee08f9f718adae3f74aa6d4b
Demmynile <balogun80@gmail.com>
2023-07-12 22:09:33 +0100

    new changes in the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import Board from "./components/Board";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
     <main>
       <Header />
-
-      <h3>Trello 2.0 Clone</h3>
+      <Board />
     </main>
   );
 }
